45d62c4d821f51644829de5764036d92ee0bb6eb
Jomael Gemota <jomael@outdoorequipped.com>
2022-01-26 08:39:00 +0800

    token validation helper

---
+++ auth/token_validation.js
@@ -0,0 +1,27 @@
+const key = require('ckey');
+const { verify } = require('jsonwebtoken');
+
+module.exports = {
+    checkToken: (req, res, next) => {
+        let token = req.get('authorization');
+
+        if(token) {
+            token = token.slice(7);
+            verify(token, key.JSTOKEN_SECRET_KEY, (err, decoded) => {
+                if(err) {
+                    res.json({
+                        success: 0,
+                        message: "You have an invalid token."
+                    });
+                } else {
+                    next();
+                }
+            })
+        } else {
+            res.json({
+                success: 0,
+                message: "You have no access."
+            });
+        }
+    }
+};
