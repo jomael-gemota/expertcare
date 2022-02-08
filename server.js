ad6347f430cc774cb851fa668d9f8cf46d9e18c2
Jomael Gemota <jomael@outdoorequipped.com>
2022-02-08 21:28:03 +0800

    heroku app deployment

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const expresss = require('express');
-const key = require('ckey');
 
 const userRouter = require('./api/user/user.router');
 const invRouter = require('./api/inv/inv.router');
 
 const app = expresss();
-const port = key.SERVER_PORT;
+const port = process.env.PORT || 5000;
 
 app.use(expresss.json());
 
 app.use('/api/user', userRouter);
 app.use('/api/inv', invRouter);
+
+if (process.env.NODE_ENV === 'production') {    
+    //set static folder    
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));    
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server Port: ${port}`);
